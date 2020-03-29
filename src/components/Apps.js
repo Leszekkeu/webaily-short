85ee686a33ca0c7800d5375e2c60b1c582412af6
Leszek Korzan <leszekk@Leszekk.eu>
2020-03-29 20:02:45 +0200

    updated

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -9,9 +9,9 @@ class Apps extends Component {
                 <div className="center-box">
                     <a className="btn dl" href="https://www.icloud.com/shortcuts/85bb275fbce74a70bf2c1a681b50f8a9" target="_blank">Siri Shortcut</a>
                     <a className="btn dl" href="https://marketplace.visualstudio.com/items?itemName=LeszekKorzan.webaily" target="_blank">Visual Studio Code Extension</a>
+                    <a className="btn dl" href="https://github.com/leszekkorzan/webAilyCLI" target="_blank">CLI (Node)</a>
                 </div>
             </div>
-
         )
     }
 }
